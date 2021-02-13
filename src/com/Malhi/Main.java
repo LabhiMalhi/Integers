9a6244e6ab7bb885dd27cf790efb6d2b4991c14d
LabhiMalhi <lovepreetwm98@gmail.com>
2021-02-13 18:47:49 -0500

    Initial commit

---
+++ src/com/Malhi/Main.java
@@ -0,0 +1,12 @@
+package com.Malhi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double var2 = 3, var1 = 6;
+        System.out.println(var1>var2);
+        System.out.println(var1==var2);
+        System.out.println(var1!=var2);
+        System.out.println(var1<var2);
+    }
+}
